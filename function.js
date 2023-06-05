0027c2908300e937c22633c02d2f4841ea17de90
shakibaTavan <101308405+shakibaTavan@users.noreply.github.com>
2023-06-05 11:27:01 +1000

    fixing pop-up flickering problem adding close f

--- function.js
+++ function.js
@@ -1,33 +1,37 @@
 function myFunction() {
-    var x = document.getElementById("navBarDisplay");
-    if (x.className === "navBar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navBar";
-    }
+  var x = document.getElementById("navBarDisplay");
+  if (x.className === "navBar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navBar";
   }
+}
 
-  function myFunction() {
-    var cut = document.getElementById("cut");
-    var moreText = document.getElementById("extra");
-    var btnText = document.getElementById("read-btn");
-  
-    if (cut.style.display === "none") {
-      cut.style.display = "inline";
-      btnText.innerHTML = "read more";
-      moreText.style.display = "none";
-    } else {
-      cut.style.display = "none";
-      btnText.innerHTML = "read less";
-      moreText.style.display = "inline";
-    }
-  }
-  
+function myFunction() {
+  var cut = document.getElementById("cut");
+  var moreText = document.getElementById("extra");
+  var btnText = document.getElementById("read-btn");
 
-  function added() {
-    var message = document.getElementById("added-message");
-    var overlay = document.getElementById("dimmer");
-    message.classList.toggle("show");
-    dimmer.classList.toggle("show");
+  if (cut.style.display === "none") {
+    cut.style.display = "inline";
+    btnText.innerHTML = "read more";
+    moreText.style.display = "none";
+  } else {
+    cut.style.display = "none";
+    btnText.innerHTML = "read less";
+    moreText.style.display = "inline";
   }
-  
+}
+
+function added() {
+  var message = document.getElementById("added-message");
+  var overlay = document.getElementById("dimmer");
+  message.classList.add("show");
+  overlay.classList.add("show");
+
+  var closeButton = document.querySelector(".close-btn");
+  closeButton.addEventListener("click", function () {
+    message.classList.remove("show");
+    overlay.classList.remove("show");
+  });
+}
